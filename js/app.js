e20222fed1821391581c36e9e609c5d834115084
Sergiu Stancioiu <s.sergiu96@gmail.com>
2023-09-21 22:00:30 +0300

    Added functionality for Filter by Region

--- js/app.js
+++ js/app.js
@@ -1,41 +1,76 @@
-const apiUrl = "https://restcountries.com/v3.1/all";
+document.addEventListener("DOMContentLoaded", () => {
+  const apiUrl = "https://restcountries.com/v3.1/all";
 
-fetch(apiUrl)
-  .then((response) => {
-    if (response.ok) {
-      return response.json();
-    } else {
-      throw new Error(`Error: ${response.status}`);
-    }
-  })
-  .then((data) => {
-    data.forEach((country) => {
+  fetch(apiUrl)
+    .then((response) => {
+      if (response.ok) {
+        return response.json();
+      } else {
+        throw new Error(`Error: ${response.status}`);
+      }
+    })
+    .then((data) => {
+      const selectedRegion = document.querySelector(".region");
       const container = document.querySelector(".countries-container");
       const template = document.querySelector("#country-template");
 
-      let cloneTemplate = template.content.cloneNode(true);
+      function populateCountries(region) {
+        container.innerHTML = "";
 
-      cloneTemplate.querySelector(".country-flag").textContent = country.flag;
+        if (region === "All") {
+          data.forEach((country) => {
+            const cloneTemplate = template.content.cloneNode(true);
+            cloneTemplate.querySelector(".country-flag").textContent =
+              country.flag;
+            cloneTemplate.querySelector(".country-heading").textContent =
+              country.name.common;
+            cloneTemplate.querySelector(".population").textContent =
+              country.population;
+            cloneTemplate.querySelector(".region").textContent = country.region;
+            cloneTemplate.querySelector(".capital").textContent =
+              country.capital;
 
-      cloneTemplate.querySelector(".country-heading").textContent =
-        country.name.common;
+            container.appendChild(cloneTemplate);
 
-      cloneTemplate.querySelector(".population").textContent =
-        country.population;
+            console.log("Selected Region Value:", region);
+          });
+        } else {
+          data.forEach((country) => {
+            if (region === country.region) {
+              const cloneTemplate = template.content.cloneNode(true);
+              cloneTemplate.querySelector(".country-flag").textContent =
+                country.flag;
+              cloneTemplate.querySelector(".country-heading").textContent =
+                country.name.common;
+              cloneTemplate.querySelector(".population").textContent =
+                country.population;
+              cloneTemplate.querySelector(".region").textContent =
+                country.region;
+              cloneTemplate.querySelector(".capital").textContent =
+                country.capital;
 
-      cloneTemplate.querySelector(".region").textContent = country.region;
+              container.appendChild(cloneTemplate);
 
-      cloneTemplate.querySelector(".capital").textContent = country.capital;
+              console.log("Selected Region Value:", region);
+            }
+          });
+        }
+      }
 
-      container.appendChild(cloneTemplate);
+      selectedRegion.addEventListener("change", (event) => {
+        const region = event.target.value;
+        populateCountries(region);
+      });
+
+      populateCountries("All");
+    })
+    .catch((error) => {
+      console.error(error);
     });
-  })
-  .catch((error) => {
-    console.error(error);
+
+  const darkModeBtn = document.querySelector(".switcher");
+
+  darkModeBtn.addEventListener("click", () => {
+    document.body.classList.toggle("dark-mode");
   });
-
-const darkModeBtn = document.querySelector(".switcher");
-
-darkModeBtn.addEventListener("click", () => {
-  document.body.classList.toggle("dark-mode");
 });
