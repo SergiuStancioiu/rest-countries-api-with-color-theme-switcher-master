ba1d93fe50dd3133cde5ce2d069541dec7db4c1c
Sergiu Stancioiu <s.sergiu96@gmail.com>
2023-10-03 20:35:58 +0300

    Worked on 'Search for a country' and 'Filter by Region' functionality

--- js/app.js
+++ js/app.js
@@ -1,6 +1,15 @@
 document.addEventListener("DOMContentLoaded", () => {
-  const apiUrl = "https://restcountries.com/v3.1/all";
+  const searchInput = document.querySelector(".search");
+  const regionSelect = document.querySelector(".region");
 
+  regionSelect.addEventListener("change", regionSelectOnChange);
+  searchInput.addEventListener("keyup", searchInputOnKeyUp);
+
+  fetchAllData();
+  enableDarkMode();
+});
+
+function fetchData(apiUrl) {
   fetch(apiUrl)
     .then((response) => {
       if (response.ok) {
@@ -10,67 +19,75 @@ document.addEventListener("DOMContentLoaded", () => {
       }
     })
     .then((data) => {
-      const selectedRegion = document.querySelector(".region");
-      const container = document.querySelector(".countries-container");
-      const template = document.querySelector("#country-template");
-
-      function populateCountries(region) {
-        container.innerHTML = "";
-
-        if (region === "All") {
-          data.forEach((country) => {
-            const cloneTemplate = template.content.cloneNode(true);
-            cloneTemplate.querySelector(".country-flag").textContent =
-              country.flag;
-            cloneTemplate.querySelector(".country-heading").textContent =
-              country.name.common;
-            cloneTemplate.querySelector(".population").textContent =
-              country.population;
-            cloneTemplate.querySelector(".region").textContent = country.region;
-            cloneTemplate.querySelector(".capital").textContent =
-              country.capital;
-
-            container.appendChild(cloneTemplate);
-
-            console.log("Selected Region Value:", region);
-          });
-        } else {
-          data.forEach((country) => {
-            if (region === country.region) {
-              const cloneTemplate = template.content.cloneNode(true);
-              cloneTemplate.querySelector(".country-flag").textContent =
-                country.flag;
-              cloneTemplate.querySelector(".country-heading").textContent =
-                country.name.common;
-              cloneTemplate.querySelector(".population").textContent =
-                country.population;
-              cloneTemplate.querySelector(".region").textContent =
-                country.region;
-              cloneTemplate.querySelector(".capital").textContent =
-                country.capital;
-
-              container.appendChild(cloneTemplate);
-
-              console.log("Selected Region Value:", region);
-            }
-          });
-        }
-      }
-
-      selectedRegion.addEventListener("change", (event) => {
-        const region = event.target.value;
-        populateCountries(region);
-      });
-
-      populateCountries("All");
+      populateCountries(data);
     })
     .catch((error) => {
       console.error(error);
     });
+}
 
+function populateCountries(data) {
+  const container = document.querySelector(".countries-container");
+  const template = document.querySelector("#country-template");
+
+  container.innerHTML = "";
+
+  data.forEach((country) => {
+    const cloneTemplate = template.content.cloneNode(true);
+    cloneTemplate.querySelector(".country-flag").textContent = country.flag;
+    cloneTemplate.querySelector(".country-heading").textContent =
+      country.name.common;
+    cloneTemplate.querySelector(".population").textContent = country.population;
+    cloneTemplate.querySelector(".region").textContent = country.region;
+    cloneTemplate.querySelector(".capital").textContent = country.capital;
+
+    container.appendChild(cloneTemplate);
+  });
+}
+
+function searchInputOnKeyUp(event) {
+  const baseURL = "https://restcountries.com/v3.1/";
+  const urlFields = ["flag", "name", "population", "region", "capital"];
+
+  let searchedCountry = event.target.value;
+  if (searchedCountry == "") {
+    fetchAllData();
+    return false;
+  }
+  const apiUrl = new URL(`name/${searchedCountry}`, baseURL);
+  apiUrl.searchParams.set("fields", urlFields);
+
+  fetchData(apiUrl);
+}
+
+function regionSelectOnChange(event) {
+  const baseURL = "https://restcountries.com/v3.1/";
+  const urlFields = ["flag", "name", "population", "region", "capital"];
+
+  let selectedRegion = event.target.value;
+  if (selectedRegion == "All") {
+    fetchAllData();
+    return false;
+  }
+  const apiUrl = new URL(`region/${selectedRegion}`, baseURL);
+  apiUrl.searchParams.set("fields", urlFields);
+
+  fetchData(apiUrl);
+}
+
+function enableDarkMode() {
   const darkModeBtn = document.querySelector(".switcher");
 
   darkModeBtn.addEventListener("click", () => {
     document.body.classList.toggle("dark-mode");
   });
-});
+}
+
+function fetchAllData() {
+  const baseURL = "https://restcountries.com/v3.1/";
+  const urlFields = ["flag", "name", "population", "region", "capital"];
+  const apiUrl = new URL("all", baseURL);
+  apiUrl.searchParams.set("fields", urlFields);
+
+  fetchData(apiUrl);
+}
